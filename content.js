efa5f595631465d240bef2b7a18381f8e7128a49
Sushant Kumar Singh <sushant.201s@gmail.com>
2020-07-03 11:31:46 +0530

    Added files to invert page style

--- content.js
+++ content.js
@@ -4,8 +4,16 @@ chrome.runtime.sendMessage({
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.todo == "changeBackground") {
-        $('html *').attr('style', 'background: pink');
-        // $('div').attr('style', 'background-color: pink !important');
+    //    $('body').attr('style', 'background-color: black');
+    //    $('body').attr('style', 'color: white');
+       $("body div").css("background-color", "black");
+       $("html *").css("color", "white");
+       
+    //    $("body div").css("color", "white");
+    //    $("body span").css("color", "white");
+    //    $("body cite").css("color", "white");
+    //    $("body a").css("color", "white");
+       
         // $('span').attr('style', 'background-color: pink !important');
     }
 })
